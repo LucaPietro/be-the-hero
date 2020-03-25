a4c8d0118cc84f64352c58643e1c81e264de5819
LucaPietro <lucabaronpietro@gmail.com>
2020-03-25 20:45:06 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://exemplo.com'
+}));
+
+
+app.use(express.json());
+
+app.use(routes);
+
+
+
+app.listen(3333);
